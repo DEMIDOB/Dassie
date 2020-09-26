4fe1eaf06925640b197e0f0f6d441e030d056404
DEMIDOB <dania.demidov@yandex.ru>
2020-09-26 11:00:11 +0300

    Setup script changed

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 import os, platform
 
+
 def setup_dassie():
     os_name = platform.system()
     def_python_command = "python" if os_name == "Windows" else "python3"
 
-    os.system(f"{def_python_command} -m venv dassie")
-    os.system("source dassie/bin/activate")
+    venv_name = "venv"
 
-    python_command = "dassie/bin/python3"
+    os.system(f"{def_python_command} -m venv {venv_name}")
+    os.system(f"source {venv_name}/bin/activate")
+
+    python_command = f"{venv_name}/bin/python3"
     os.system(f"{python_command} -m pip install -r requirements.txt")
     os.system(f"{python_command} -m pip freeze")
 
     import colorama
     colorama.init()
     print()
-    print(colorama.Back.GREEN + "All the requirements are satisfied." + colorama.Back.BLACK)
+    print(colorama.Fore.GREEN + "All the requirements are satisfied.")
     print()
     print(colorama.Fore.GREEN + f"To start your work with Dassie run '{def_python_command} dassie.py'")
 
